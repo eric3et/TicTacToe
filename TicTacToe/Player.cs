13c9d3fa63dbe6733dee76d27042d4173e043d1c
eric3 <eric3@DESKTOP-U0MCNKH>
2021-07-23 09:00:44 -0700

    Add project files.

---
+++ TicTacToe/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+	public class Player
+	{
+		// X's or O's
+		public string team { get; set; }
+		public string enemy { get; set; }
+
+		//used to keep track of players move before pressing submit(by butten.name), in the case that the player
+		//wants to switch positions the old position will be cleared out.
+		public string currentMove { get; set; }
+
+	   
+
+
+		public Player(string team, string enemy)
+		{
+			this.team = team;
+			this.enemy = enemy;
+			currentMove = " ";
+		}
+
+
+
+	}
+}
